75dfb89212d5c9c6bc233246b9dbfb7661401ab1
matheushenriquefs <matheus.henrique1021@gmail.com>
2024-08-20 00:07:26 -0300

    style: run lint

--- app/factories/minha_cdn_log_factory.ts
+++ app/factories/minha_cdn_log_factory.ts
@@ -1,6 +1,10 @@
 import { MinhaCDNLogExtractor } from '#adapters/minha_cdn_log_extractor'
 import { MinhaCDNLogReader } from '#adapters/minha_cdn_log_reader'
-import { type LogExtractorContract, type LogFactoryContract, type LogReaderContract } from '#types/log'
+import {
+  type LogExtractorContract,
+  type LogFactoryContract,
+  type LogReaderContract,
+} from '#types/log'
 
 export class MinhaCDNLogFactory implements LogFactoryContract {
   createReader(): LogReaderContract {
